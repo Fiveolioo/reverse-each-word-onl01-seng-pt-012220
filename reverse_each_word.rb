a59bb7d8a831bac926d65e435ab6ad27ffbf9eda
Richard Cho <96richardohc@gmail.com>
2020-02-08 07:37:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(sentence1)
-  sentence = sentence1.split(' ')
-  reverse_words = sentence.collect do |word|
+  reverse_words = sentence1.split(' ').collect do |word|
     word.reverse
   end
   reverse_words.join(' ')
